dd3b2254a7fa3f389c6c651177018b59e7229769
yztg676k7r-arch <yztg676k7r@privaterelay.appleid.com>
2026-07-29 12:15:44 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='gewinnen-v3-20260729';
-const ASSETS=['./','./index.html','./styles.css?v=3','./app.js?v=3','./manifest.webmanifest?v=3','./icons/icon-180.png?v=3','./icons/icon-192.png?v=3','./icons/icon-512.png?v=3'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.pathname.endsWith('/data/contests.json')){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>new Response('{"contests":[]}',{headers:{'Content-Type':'application/json'}})));return}e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
