e8c45de40a95061b3708c30e3339bc96c005cc75
Blaue.Elise <yztg676k7r@privaterelay.appleid.com>
2026-07-29 20:36:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='win-win-1.5.0';
+const CACHE='win-win-1.5.1';
 const CORE=[
  './','./index.html','./styles.css','./app.js','./manifest.webmanifest',
  './apple-touch-icon.png','./icons/icon-180.png','./icons/icon-192.png',
